47b1c0f7b5b53ea3f21f86e3e67cd923b26658ba
John Pavley <jpavley@gmail.com>
2020-01-18 10:45:07 -0500

    handle negative inoyt:

--- iseven.js
+++ iseven.js
@@ -16,3 +16,7 @@ console.log("=== isEven()Tests ===");
 for (let i = 0; i < 10; i++) {
     console.log(`${i} is ${isEven(i) ? "even":"odd"}`);
 }
+
+console.log(`${50} is ${isEven(50) ? "even":"odd"}`);
+console.log(`${75} is ${isEven(75) ? "even":"odd"}`);
+console.log(`${-1} is ${isEven(-1) ? "even":"odd"}`);
