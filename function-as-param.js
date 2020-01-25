febc24b308107dea4a120881e31c35d60776e82e
John Pavley <jpavley@gmail.com>
2020-01-25 12:08:15 -0500

    comments

--- function-as-param.js
+++ function-as-param.js
@@ -1,6 +1,9 @@
 // function-as-param
 
 function greaterThan(n) {
+    // the param n is "captured" buy the closure such that when invoked (apply?)
+    // a function is returned with n "hard coded" to the value passed in when the
+    // parent function was invoked.
     return (m) => m > n;
 }
 
