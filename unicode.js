e7b721ef1b685df9c1721c142591d2611048addb
John Pavley <jpavley@gmail.com>
2020-01-27 07:53:24 -0500

    filtering

--- unicode.js
+++ unicode.js
@@ -3,6 +3,18 @@
 let langInfo = require('./unicode-scripts.js');
 console.log(langInfo[0], langInfo.length);
 
+function filter(array, test) {
+    let result = [];
+    for (let element of array) {
+        if (test(element)) {
+            result.push(element);
+        }
+    }
+    return result;
+}
+
+console.log("living:", filter(langInfo, lang => lang.living).length);
+console.log("dead:", filter(langInfo, lang => !lang.living).length);
 
 
 
