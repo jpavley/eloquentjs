38c9d25d7b89d65df5bae9d89f43e24064b7316f
John Pavley <jpavley@gmail.com>
2020-01-28 15:17:10 -0500

    recursive flat

---
+++ flatten.js
@@ -0,0 +1,19 @@
+// flatten
+
+function flatten(o) {
+    let result = [];
+
+    o.forEach(e => {
+        if (Array.isArray(e)) {
+            result.push(...flatten(e));
+        } else {
+            result.push(e);
+        }
+    });
+
+    return result;
+}
+
+console.log(flatten([[1,2,3],4,5,[6,7,8]]));
+console.log(flatten([[1,2,3],[4],5,[6,[7,8]]]));
+console.log(flatten([[1,[2,3]],[4],5,[6,[[7],8]]]));
