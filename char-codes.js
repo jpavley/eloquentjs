743115577ec28cf00a0a0a680bd19ff83f46cf37
John Pavley <jpavley@gmail.com>
2020-01-28 08:33:21 -0500

    textScripts()

--- char-codes.js
+++ char-codes.js
@@ -3,7 +3,7 @@ let langInfo = require('./unicode-scripts.js');
 // code: a character code
 // returns: the name of the language of which this char code is 
 // a member or null if no lang is found
-function characterLanguage(code) {
+function characterScript(code) {
     for (let lang of langInfo) {
         if (lang.ranges.some(([from, to]) => {
             return code >= from && code < to;
@@ -16,14 +16,12 @@ function characterLanguage(code) {
 }
 
 // should return Latin
-console.log(characterLanguage(121).name);
+console.log(characterScript(121).name);
 
 // should return Han
-console.log(characterLanguage(11904).name);
+console.log(characterScript(11904).name);
 
 let freezingMindBlown = "🥶🤯";
-console.log(freezingMindBlown);
-
 for (let char of freezingMindBlown) {
     console.log(char);
 }
@@ -35,7 +33,7 @@ for (let char of freezingMindBlown) {
 function countBy(items, groupName) {
     let counts = [];
     for (let item of items) {
-        let name = groupName(item);
+        let name = groupName(item); // is item in group?
         let known = counts.findIndex(c => c.name == name);
         if (known == -1) {
             counts.push({name, count: 1});
@@ -49,4 +47,22 @@ function countBy(items, groupName) {
 console.log(countBy([1,2,3,4,5], n => n > 2));
 console.log(countBy(["a", "b", "c"], c => c != "a" ? "not a" : "a"));
 console.log(countBy([0,1,2,3,4,5,6,7,8,9], n => n % 2 === 0 ? "even" : "odd"));
+console.log(countBy([1,2,1,2,1,2], n => n * n));
+
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.name : "none";
+    }).filter(({name}) => name != "none");
+
+    // compute total number of chars that belong to a script
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+
+    return scripts.map(({name, count}) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
 
