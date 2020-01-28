4a563ddbe0f5367839dde943846b85e0a256a917
John Pavley <jpavley@gmail.com>
2020-01-28 08:14:43 -0500

    added countBy()

--- char-codes.js
+++ char-codes.js
@@ -1,5 +1,8 @@
 let langInfo = require('./unicode-scripts.js');
 
+// code: a character code
+// returns: the name of the language of which this char code is 
+// a member or null if no lang is found
 function characterLanguage(code) {
     for (let lang of langInfo) {
         if (lang.ranges.some(([from, to]) => {
@@ -24,3 +27,26 @@ console.log(freezingMindBlown);
 for (let char of freezingMindBlown) {
     console.log(char);
 }
+
+// items: a collection (for/of)
+// groupName: function that computes group name for an element
+// returns: an array of objects, each object includes a group
+// name and the number of elements found in that group
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({name, count: 1});
+        } else {
+            counts[known].count++;
+        }
+    }
+    return counts;
+}
+
+console.log(countBy([1,2,3,4,5], n => n > 2));
+console.log(countBy(["a", "b", "c"], c => c != "a" ? "not a" : "a"));
+console.log(countBy([0,1,2,3,4,5,6,7,8,9], n => n % 2 === 0 ? "even" : "odd"));
+
