1f7d58a9dd20e5782082f18a309a908a9be50432
John Pavley <john@pavley.com>
2020-02-02 09:51:33 -0500

    inheritence

--- Matrix.js
+++ Matrix.js
@@ -68,3 +68,26 @@ console.log(matrix);
 for (let {x, y, value} of matrix) {
     console.log(x, y, value);
 }
+
+class SymmetricMatrix extends Matrix {
+
+    constructor(size, element = (x, y) => undefined) {
+        super(size, size, (x, y) => {
+            if (x < y) {
+                return element(y, x);
+            } else {
+                return element(x, y);
+            }
+        });
+    }
+
+    set(x, y, value) {
+        super.set(x, y, value);
+        if (x != y) {
+            super.set(y, x, value);
+        }
+    }
+}
+
+let sm = new SymmetricMatrix(5, (x, y) => `sm ${x},${y}`);
+console.log(sm.get(2,3));
