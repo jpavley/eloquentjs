971d0a9d4403755f3e1511ccb3dcb00a6be4c380
John Pavley <jpavley@gmail.com>
2020-02-18 16:26:36 -0500

    Update reg-express.js

--- reg-express.js
+++ reg-express.js
@@ -32,3 +32,26 @@ print("(808) 123-579", phoneNumber.test("(808) 123-579"));
 let notBinary = /[^01]/;
 print("11001100101010101",notBinary.test("11001100101010101"));
 print("11001100121010101",notBinary.test("11001100121010101"));
+
+print("\\d+", /\d+/.test("123"));
+print("\\d+", /\d+/.test(""));
+print("\\d*", /\d*/.test("123"));
+print("\\d*", /\d*/.test(""));
+
+let rel4 = /neighbou?r/;
+print("neighbour", rel4.test("neighbour"));
+print("neighbor", rel4.test("neighbor"));
+
+let dateTime2 = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
+print(dateTime2.test("1-30-2003 8:45"));
+print(dateTime2.test("01-30-2003 08:45"));
+print(dateTime2.test("01-1-2003 08:45"));
+print(dateTime2.test("01-1-03 08:45"));
+
+let cartoonCrying = /boo+(hoo+)+/i;
+print("booooooHoooooHOOOOOhoo", cartoonCrying.test("booooooHoooooHOOOOOhoo"));
+
+
+
+
+
